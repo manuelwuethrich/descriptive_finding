c06df5ee9d7cb91328b7861566dd4abf389df663
manuelwuethrich <manuelwuthrich@Manuels-MacBook-Pro.local>
2020-04-20 13:44:55 +0200

    finished.

--- descriptive_finding.R
+++ descriptive_finding.R
@@ -1,23 +1,16 @@
 # packages
 
-# install packages (once)
-install.packages("rio")        # for importing
-install.packages("dplyr")      # for cleaning
-install.packages("stargazer")  # for tabling
-install.packages("ggplot2")    # for plotting
-
-# load packages (every R session)
 library(rio)
 library(dplyr)
 library(stargazer)
 library(ggplot2)
-# -------------------------------------------------------------------------
+
+# data
 
 data <- import("CPDS-1960-2017-Update-2019.xlsx")
 
 # filter
 
-# filter
 filtered.data <- data %>%             
   filter(year == "2017")   # limit it to observations from 2017
 
@@ -26,8 +19,8 @@ filtered.data <- data %>%
 ggplot(data=filtered.data, aes(gov_type)) + 
 geom_histogram(binwidth = 0.5,
                col="black",
-               fill="grey",
-               ) +
+               fill="grey") +
   labs(title="Histogram of Government Types in 2017",
        x="Government Types",
-       y="Number of Countries")
+       y="Number of Countries") +
+  scale_y_continuous(breaks = seq (0, 15, by = 1))
